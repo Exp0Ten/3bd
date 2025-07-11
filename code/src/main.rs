7c55d4f07cb819bf22d9e88580524628ff881a1b
auroradeb <ad.zboril@gmail.com>
2025-07-11 14:46:12 +0200

    Code structure

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,23 @@
+/*
+    This is the main file of the entire program
+    It's responsible for all modules to be working together correctly
+    It handles errors, logging as feedback to development and the user, also is used as specification for all of the modules
+    It handles the runtime, window, and debugging routine
+*/
+use std::fs;
+use syscalls;
+use iced;
+
+
+mod test;       // for testing and debbuging
+
+// the following files go in sequence based on what they depend on (tracing need objdump, data needs trace, ui needs data ...)
+mod objdump;    // ELF handling, reading, preparing, AND TRACING THE PROGRAM (trace.rs merily debugs it)
+mod trace;      // debugging programs
+mod data;       // data manipulation, reading
+mod ui;         // user interface - communication with user, preferences, config ...
+mod window;     // window and graphics handling
+
 fn main() {
     println!("Hello, world!");
 }
