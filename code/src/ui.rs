17bbc676d126a74aba3f8e9f0fe5c0605926f719
azi_kufr <ad.zboril@gmail.com>
2025-11-04 23:48:15 +0100

    Diverging from resize No custom titlebar

--- code/src/ui.rs
+++ code/src/ui.rs
@@ -1,5 +1,5 @@
 use iced::{
-    Length, Task, mouse::Interaction, 
+    Task,
     widget::{
         Container, MouseArea, Row, Text, Theme,
         button, column, container, mouse_area, pane_grid, row, svg, text,
@@ -34,68 +34,6 @@ pub fn content(state: &State) -> Container<'_, Message> {
     )
 }
 
-
-
-pub fn titlebar(app: &App) -> Container<'_, Message> {
-    container(row![
-        icon(),
-        title(),
-        winbuttons(&app.state)
-    ])
-    //needs styling
-    .into()
-}
-
-fn icon<'a>() -> Svg<'a, Theme> {
-    Svg::new(Handle::from_memory(
-        Asset::get("icons/TBD.svg").unwrap().data)
-    )
-    //needs styling
-}
-
-fn title<'a>() -> Text<'a, Theme> {
-    text({
-        match &INTERNAL.access().file {
-            Some(path) =>  format!("{} - Three Body Debugger", path.file_name().unwrap().to_str().unwrap()),
-            None => "Three Body Debugger".to_string()
-        }
-    })
-    //needs styling
-}
-
-fn winbuttons<'a>(state: &State) -> Row<'a, Message> {
-    let minbutton = button(
-        svg(Handle::from_memory(
-            Asset::get("icons/window_minimize.svg").unwrap().data
-        ))
-        //needs styling
-    ).on_press(Message::Window(WinMessage::Minimize));
-
-    let maxbutton = button(
-        svg(Handle::from_memory(
-            Asset::get(
-                if state.maximized {"icons/window_restore.svg"} else {"icons/window_maximize.svg"}
-            ).unwrap().data
-        ))
-        //needs styling
-    ).on_press(if state.maximized {Message::Window(WinMessage::Restore)} else {Message::Window(WinMessage::Maximize)});
-
-    let xbutton = button(
-        svg(Handle::from_memory(
-            Asset::get("icons/window_close.svg").unwrap().data
-        ))
-        //needs styling
-    ).on_press(Message::Window(WinMessage::Close));
-
-
-    row![
-        minbutton,
-        maxbutton,
-        xbutton
-    ]
-    //needs styling
-}
-
 pub fn pane_message(state: &mut State, pane: PaneMessage) -> Task<Message> {
     let task: Option<Task<Message>> = match pane {
         //fill in later
@@ -105,59 +43,5 @@ pub fn pane_message(state: &mut State, pane: PaneMessage) -> Task<Message> {
     match task {
         Some(task) => task,
         None => Task::none()
-    }
-}
-
-pub fn resize_area<'a>(width: u8) -> Container<'a, Message> {
-    container(
-        row![
-            column![
-                mouse_area(container(" ").height(Length::Fixed(width.into())).width(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::TopLeft),
-                mouse_area(container(" ").center_y(Length::Fill))
-                .mouse_resize_handle(Direction::Left),
-                mouse_area(container(" ").height(Length::Fixed(width.into())).width(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::BottomLeft)
-                ]
-            .width(Length::Fixed(width.into())),
-            column![
-                mouse_area(container(" ").center_x(Length::Fill).height(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::Top),
-                container(" ").center(Length::Fill),
-                mouse_area(container(" ").center_x(Length::Fill).height(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::Bottom)
-            ]
-            .width(Length::Fill),
-            column![
-                mouse_area(container(" ").height(Length::Fixed(width.into())).width(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::TopRight),
-                mouse_area(container(" ").center_y(Length::Fill))
-                .mouse_resize_handle(Direction::Right),
-                mouse_area(container(" ").height(Length::Fixed(width.into())).width(Length::Fixed(width.into())))
-                .mouse_resize_handle(Direction::BottomRight)
-            ]
-            .width(Length::Fixed(width.into()))
-        ]
-        .height(Length::Fill)
-        .width(Length::Fill)
-    )
-}
-
-trait ResizeHandle {
-    fn mouse_resize_handle(self, direction: Direction) -> Self;
-}
-
-impl ResizeHandle for MouseArea<'_, Message> {
-    fn mouse_resize_handle(self, direction: Direction) -> Self {
-        self
-        .on_press(Message::Window(WinMessage::ResizeStart(direction.clone())))
-        .on_move(|point| Message::Window(WinMessage::ResizeMove(point)))
-        .on_release(Message::Window(WinMessage::ResizeDone))
-        .interaction(match direction {
-            Direction::Top | Direction::Bottom => Interaction::ResizingVertically,
-            Direction::Left | Direction::Right => Interaction::ResizingHorizontally,
-            Direction::TopLeft | Direction::BottomRight => Interaction::ResizingDiagonallyDown,
-            Direction::BottomLeft | Direction::TopRight => Interaction::ResizingDiagonallyUp
-        })
     }
 }
--- code/src/window.rs
+++ code/src/window.rs
@@ -1,5 +1,5 @@
 use iced::{
-    Element, Point, Size, Task, Theme,
+    Element, Theme, Task,
     application, window,
     widget::{
         column, stack
@@ -20,58 +20,23 @@ pub struct App {
 
 pub struct State {
     //    panes: pane_grid::State<Pane>
-    resizing: Option<Resize>,
-    pub maximized: bool,
     // add more as needed
 }
 
 impl Default for State {
     fn default() -> Self {
         State {
-            resizing: None,
-            maximized: false
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    Window(WinMessage),
     Pane(PaneMessage),
     Operation(trace::Operation),
     None
     // add more as needed
 }
-
-
-#[derive(Debug, Clone)]
-pub enum WinMessage {
-    ResizeStart(Direction),
-    ResizeMove(iced::Point),
-    ResizeDone,
-    Close,
-    Maximize,
-    Restore,
-    Minimize
-}
-
-struct Resize {
-    start: Option<Point>,
-    direction: Direction
-}
-
-#[derive(Debug, Clone)]
-pub enum Direction {
-    TopLeft,
-    Left,
-    BottomLeft,
-    Top,
-    Bottom,
-    TopRight,
-    Right,
-    BottomRight
-}
-
 
 pub fn run_app() -> iced::Result {
     application("Three Body Debugger", App::update, App::view)
@@ -89,7 +54,7 @@ impl App {
             state: State::default(),
             theme: Theme::Dark,
             settings: window::Settings {
-                decorations: false,
+                decorations: true,
                 ..Default::default()
             }
         }
@@ -99,7 +64,6 @@ impl App {
         let state = &mut self.state;
 
         let task: Task<Message> = match message {
-            Message::Window(window) => window_message(self, window),
             Message::Operation(operation) => {trace::operation_message(operation); Task::none()},
             Message::Pane(pane) => pane_message(state, pane),
             _ => Task::none()
@@ -114,24 +78,7 @@ impl App {
 
         let content = content(state);
 
-        let result: Element<'_, Message> = if self.settings.decorations {
-            content.into()
-        } else {
-            let titlebar = titlebar(&self);
-            let display = column![titlebar, content];
-            match state.maximized {
-                false => {
-                    stack([
-                        resize_area(15).into(),
-                        display.into()
-                    ]).into()
-                }
-
-                true => display.into()
-            }
-        };
-
-        result
+        content.into()
     }
 
     fn theme(&self) -> Theme {
@@ -142,69 +89,4 @@ impl App {
         self.settings.clone()
     }
 
-}
-
-
-fn window_message(app: &mut App, window: WinMessage) -> Task<Message> {
-    match window {
-        WinMessage::ResizeStart(direction) => {
-            app.state.resizing = Some(Resize {start: None, direction: direction.clone()}); Task::none()
-        },
-        WinMessage::ResizeMove(point) =>
-            if let Some(Resize {direction, start}) = &mut app.state.resizing {
-                match start {
-                    None => *start = Some(point),
-                    _ => {}
-                };
-                resize(direction, start.expect("why are you"), point)
-            }
-            else {Task::none()}
-        ,
-        WinMessage::ResizeDone => {app.state.resizing = None; Task::none()},
-        _ => Task::none()
-    }
-}
-
-fn resize(direction: &Direction, start: Point, point: Point) -> Task<Message> {
-
-    struct Window {
-        position: Option<Point>,
-        size: Option<Size>
-    }
-
-    impl Window {
-        fn new(position: Option<Point>, size: Option<Size>) -> Self {
-            Self{position, size}
-        }
-
-        fn position(position: Option<Point>) -> Self {
-            Self{position, size: None}
-        }
-
-        fn size(size: Option<Size>) -> Self {
-            Self{position: None, size}
-        }
-    }
-
-    window::get_latest().and_then(|id| {
-        Task::batch([
-            window::get_position(id).map(|position| Window::position(position)).collect(),
-            window::get_size(id).map(|size| Window::size(Some(size))).collect()
-        ])
-        .then(|vector| {
-            let mut window = Window::new(vector[0].position, vector[1].size);
-
-            window = match direction {
-                Direction::Bottom => {
-                    Window::size()
-                },
-            };
-
-            match window.position {
-                Some(_) => todo!(),
-                None => todo!()
-            };
-            Task::done(Message::None)
-        })
-    })
 }
